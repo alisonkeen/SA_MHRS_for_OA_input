037fe4a9fba42654896d0c3c52d65a53137bee6a
Alison Keen <ali.keen@gmail.com>
2016-09-15 04:02:20 +0000

    Shrunk down output to match people.csv in OpenAustralia data folder

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,62 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# This script started off as a shamelessly plagiarized version of the Popolo/ EveryPolitician Virgin Islands example scraper (thanks, guys!!)
+# Hopefully it isn't too similar but it gave me a very readable starting point, as a ruby beginner. :) 
+# - Alison
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Original is here: 
+# https://github.com/tmtmtmtm/us-virgin-islands-legislature/blob/master/scraper.rb
+# And then became my other SA scraper, then this one. 
+
+require 'scraperwiki'
+require 'nokogiri' # Mechanize will have to wait for next week
+require 'open-uri'
+
+# This is stopping me from debugging/running
+# what do I need these for? 
+
+# require 'colorize'
+# require 'pry'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+
+sa_mps_url = 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/Memberlist.aspx'
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  noko.css('table table table tr').css('a[href*="MemberDrill"]').each do |a|
+    mp_url = URI.join url, a.attr('href')
+    add_person_to_list(a.text, mp_url)
+  end
+end
+
+def add_person_to_list(mp_name, url)
+
+   noko = noko_for(url)
+
+   puts "\nName: " + mp_name.to_s
+
+   # This is expected in the OpenAustarlia/TWFY import script, 
+   # in the interests of not changing anything more than I need to, here is
+   # a blank field for backwards-compatibility... 
+   mp_birthday = ""
+
+   data = { 
+     id: url.to_s.split("=").last,
+     id__saparl: url.to_s.split("=").last,
+     full_name: mp_name,
+     birthday: mp_birthday
+   }
+
+   ScraperWiki.save_sqlite([:id], data)
+end
+
+scrape_list(sa_mps_url)
+
+#Testing: just try it on one page first... 
+#scrape_person('Frances Bedford', 'https://www2.parliament.sa.gov.au/Internet/DesktopModules/MemberDrill.aspx?pid=543')
